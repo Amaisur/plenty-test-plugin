7a836b48593038ca4209ef5b8a20f5e01c8df1a9
Umais <amaisurr@outlook.com>
2026-07-29 22:27:05 +0500

    hero

--- src/Controllers/DemoHomeController.php
+++ src/Controllers/DemoHomeController.php
@@ -3,20 +3,19 @@
 namespace PlentyTestPlugin\Controllers;
 
 use Plenty\Plugin\Controller;
-use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Templates\Twig;
 use PlentyTestPlugin\Configs\HeaderConfig;
 use PlentyTestPlugin\Configs\HeroConfig;
 
 class DemoHomeController extends Controller
 {
-    public function show(Twig $twig, ConfigRepository $config)
+    public function show(Twig $twig)
     {
         return $twig->render('PlentyTestPlugin::content.Home', [
             'header' => HeaderConfig::get(),
             'hero' => HeroConfig::get(),
-            'searchAction' => $config->get('PlentyTestPlugin.searchAction', '/search'),
-            'searchParam' => $config->get('PlentyTestPlugin.searchParam', 'query'),
+            'searchAction' => '/search',
+            'searchParam' => 'query',
         ]);
     }
 }
